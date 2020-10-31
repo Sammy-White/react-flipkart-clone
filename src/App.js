9d02f37737ed2729626043d809aaf822f2e501b5
Samson Ugwu <samsonugwu802@gmail.com>
2020-10-31 12:33:41 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import CartPage from './Flipkart/Components/cart_page/cart';
+import HomePage from './Flipkart/Components/Home_page/home'
+import NavBar from './Flipkart/Components/Navbar/nav';
+import ProductPage from './Flipkart/Components/Product_page/product';
+import ShopPage from './Flipkart/Components/Shop_page/shop';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+	
+	render(){
+		return(
+			<div>
+				<Router>
+					<div>
+					<NavBar />
+						<Switch>
+							<Route path="/" exact component={HomePage}  />
+							<Route path="/shop" component={ShopPage} />
+							<Route path="/product/:id" component={ProductPage} />
+							<Route path="/cart/:id" component={CartPage} />
+						</Switch>
+					</div>
+				</Router>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
+
+
+
+
